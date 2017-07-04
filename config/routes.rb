9180528d232544625142951f0aa76119a609e3fe
Chris Palmer <cpalmer608@gmail.com>
2017-07-04 14:10:22 -0500

    action mailer email implemented

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
 
   get 'about' => 'welcome#about'
-
+  
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
